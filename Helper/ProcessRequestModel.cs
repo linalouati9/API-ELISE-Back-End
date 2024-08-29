f34e1324aec8d7027c9e82c73631c9dc85889c5c
linalouati9 <101464741+linalouati9@users.noreply.github.com>
2024-08-29 18:29:56 +0100

    Add files via upload

---
+++ Helper/ProcessRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace api_elise.Helper
+{
+    public class ProcessRequestModel
+    {
+        [Required]
+        public string Request { get; set; }
+
+        public string? XmlDocument { get; set; } // Nullable string
+    }
+
+}
